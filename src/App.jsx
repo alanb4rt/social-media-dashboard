f2c76e2b8514bf53a2244248ea2174b5398462ca
alanb4rt <al4n.b4rt@gmail.com>
2024-09-24 11:55:24 +0200

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="w-full max-w-screen-lg mx-auto p-4">
+        <header className="flex justify-between items-center gap-8 py-4">
+          <div>
+            <h1 className="text-3xl font-bold">Social Media Dashboard</h1>
+            <p>Total Followers: 23,004</p>
+          </div>
+          <div>Dark Mode</div>
+        </header>
+      </div>
     </>
   );
 }
