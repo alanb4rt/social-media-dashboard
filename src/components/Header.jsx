5f887813011873dc9a95cdbe9b2afbd7806f377f
alanb4rt <al4n.b4rt@gmail.com>
2024-09-27 23:52:53 +0200

    create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center gap-8 py-4 mb-8">
+      <div>
+        <h1 className="text-3xl font-bold">Social Media Dashboard</h1>
+        <p className="w-fit text-[var(--color-text-secondary)]">
+          Total Followers: 23,004
+        </p>
+      </div>
+      <div className="w-fit text-[var(--color-text-secondary)]">Dark Mode</div>
+    </header>
+  );
+}
