8b7f904d805e2ce898f723bbb463419e2e3b1070
alanb4rt <al4n.b4rt@gmail.com>
2024-09-30 13:41:28 +0200

    create totalFollowers for dynamic data and change followers data values

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import ToggleButton from "./ToggleButton";
+import { followers } from "../data/followers";
 
 export default function Header() {
+  const totalFollowers = followers.reduce(
+    (acc, { followers: followersCount }) => acc + followersCount,
+    0
+  );
+
   return (
     <header className="flex justify-between items-center gap-8 py-4 mb-8">
       <div>
         <h1 className="text-3xl font-bold">Social Media Dashboard</h1>
         <p className="w-fit text-[var(--color-text-secondary)]">
-          Total Followers: 23,004
+          Total Followers: {totalFollowers}
         </p>
       </div>
       <ToggleButton />
--- src/data/followers.js
+++ src/data/followers.js
@@ -1,6 +1,6 @@
 export const followers = [
     { platform: 'facebook', username: '@nathanf', followers: 1987, todayChange: 12, color: "hsl(208 92% 53%)" },
     { platform: 'twitter', username: '@nathanf', followers: 1044, todayChange: 99, color: "hsl(203 89% 53%)" },
-    { platform: 'instagram', username: '@realnathanf', followers: 11000, todayChange: 1099, color: "linear-gradient(to right, hsl(37 97% 70%), hsl(329 70% 58%))" },
+    { platform: 'instagram', username: '@realnathanf', followers: 11734, todayChange: 1099, color: "linear-gradient(to right, hsl(37 97% 70%), hsl(329 70% 58%))" },
     { platform: 'youtube', username: 'Nathan F.', followers: 8239, todayChange: -144, color: "hsl(348 97% 39%)" },
 ]
